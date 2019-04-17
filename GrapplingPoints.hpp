5e606f7ef0ebba06fba12854eb0833a73fe68854
scsec <marmiteisgood@gmail.com>
2019-04-16 17:30:01 -0700

    Updated GrapplingPoints.hpp

--- GrapplingPoints.hpp
+++ GrapplingPoints.hpp
@@ -1,22 +1,19 @@
 #pragma once
-#include <SDL2/SDL.h>
-
 #include "Entity.hpp"
 #include "SpriteSheet.hpp"
+#include "GrapplingPoint.hpp"
 #include <list>
 
 class GrapplingPoints : public Entity {
-	private:
-  std::list<GrapplingPoints> m_grappling_points;
-	public:
-		GrapplingPoints();
-    void addPoint(SDL_Point loc); // initialize location
-    void render(SDL_Renderer *renderer);
-    void update();
-    struct GrapplingPoint{
-      SDL_Point m_location;
-      SDL_Rect m_bbox;
-      SpriteSheet *m_image;
-    };
+private:
+	std::list<GrapplingPoint> m_grappling_points;
+public:
+	GrapplingPoints();
+	GrapplingPoints(std::list<GrapplingPoint>);
+	~GrapplingPoints();
+	void addPoint(SDL_Point loc); // initialize location
+	void render(SDL_Renderer*);
+	void update();
 };
+
 
