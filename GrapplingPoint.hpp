511bfd5d94f09d887b731a3994bee758c75b58eb
scsec <marmiteisgood@gmail.com>
2019-04-17 16:54:32 -0700

    Update GrapplingPoint.hpp

--- GrapplingPoint.hpp
+++ GrapplingPoint.hpp
@@ -1,8 +1,13 @@
 #pragma once
-#include "GrapplingHook.hpp"
+#include "Entity.hpp"
+#include "SpriteSheet.hpp"
 
 class GrapplingPoint : public Entity {
+private:
 	SDL_Point m_location;
 	SDL_Rect m_bbox;
-	SpriteSheet* m_image;
+	SpriteSheet m_image;
+public:
+	GrapplingPoint();
+	GrapplingPoint(SDL_Point, SDL_Rect, SpriteSheet);
 };
