639785f65891b2e237fe5c4daf02ec20a7ac9f01
scsec <marmiteisgood@gmail.com>
2019-04-17 16:57:05 -0700

    Update Obstacle.hpp

--- Obstacle.hpp
+++ Obstacle.hpp
@@ -1,7 +1,13 @@
+#pragma once
 #include "SpriteSheet.hpp"
+#include <SDL2/SDL.h>
 
 class Obstacle
 {
+private:
 	SDL_Rect m_bbox;
 	SpriteSheet m_sprite;
+public:
+	Obstacle();
+	Obstacle(SDL_Rect, SpriteSheet);
 };
