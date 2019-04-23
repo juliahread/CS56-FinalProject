9a6424e792399af866d536abfe86d7df1eab9956
Janice Lee <jlad2017@mymail.pomona.edu>
2019-04-22 22:03:36 -0700

    updated to include SDL_TTF

---
+++ SDLHelper.cpp
@@ -0,0 +1,90 @@
+#include "SDLHelper.hpp"
+
+SDLHelper::SDLHelper(const int screen_width, const int screen_height, char* window_name):SCREEN_WIDTH(screen_width), SCREEN_HEIGHT(screen_height), WINDOW_NAME(window_name) {
+  bool initialized = SDLHelper::init();
+  if (not initialized) {
+    throw std::runtime_error("SDLHelper failed to initialize");
+  }
+}
+
+SDLHelper::~SDLHelper() {
+  SDLHelper::close();
+}
+
+bool SDLHelper::init() {
+	//Initialization flag
+	bool success = true;
+	//Initialize SDL
+	if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else {
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+		//Create window
+		window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+		if (window == NULL) {
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else {
+			//Create renderer for window
+			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+			if (renderer == NULL) {
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else {
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags)) {
+					printf("SDL_image could not initialize! SDL_Image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+                //Initialize SDL_ttf
+                if (TTF_Init() == -1) {
+                    printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+                    success = false;
+                }
+			}
+		}
+	}
+
+	return success;
+}
+
+void SDLHelper::close() {
+    //Free global font
+    TTF_CloseFont(font);
+    font = NULL;
+
+	//Destroy window
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	window = NULL;
+	renderer = NULL;
+
+	//Quit SDL subsystems
+    TTF_Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int SDLHelper::getScreenWidth() const{
+  return SCREEN_WIDTH;
+}
+
+int SDLHelper::getScreenHeight() const{
+  return SCREEN_HEIGHT;
+}
+
+char* SDLHelper::getWindowName() const{
+  return WINDOW_NAME;
+}
---
+++ SDLHelper.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef _WIN32
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#else
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+#endif
+#include <string>
+#include <stdexcept>
+
+/*
+ * SDLHelper class
+ * Holds the main window, renderer, and helper functions pertaining to them
+ */
+class SDLHelper{
+public:
+  SDLHelper(const int SCREEN_WIDTH, const int SCREEN_HEIGHT, char* WINDOW_NAME);
+  ~SDLHelper();
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  TTF_Font *font;
+  int getScreenWidth() const;
+  int getScreenHeight() const;
+  char *getWindowName() const;
+private:
+  bool init();
+  void close();
+  const int SCREEN_WIDTH;
+  const int SCREEN_HEIGHT;
+  char *WINDOW_NAME;
+};
