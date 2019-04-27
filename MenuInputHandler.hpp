be13f64983defb3b16fec746617e45e8d6288703
Sean Costello <marmiteisgood@gmail.com>
2019-04-26 17:30:30 -0700

    Correct map.png format

--- MenuInputHandler.hpp
+++ MenuInputHandler.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include "Command.hpp"
+#include "SDLHelper.hpp"
 
 class MenuInputHandler {
-private:
-    Command *m_menu_w_button;
-    Command *m_menu_s_button;
-    Command *m_menu_enter_button;
+ private:
+  Command *m_menu_w_button;
+  Command *m_menu_s_button;
+  Command *m_menu_enter_button;
 
-public:
-    MenuInputHandler();
-    ~MenuInputHandler();
-    Command *handle_input(SDL_Event e, bool game);
-}
+ public:
+  MenuInputHandler();
+  ~MenuInputHandler();
+  Command *handle_input(SDL_Event e, bool game);
+};
