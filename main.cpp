1db83c6b6c7242c310470015d1bcb6ea96ee66df
Janice Lee <jlad2017@mymail.pomona.edu>
2019-04-28 21:55:34 -0700

    small changes to menu input handling

--- main.cpp
+++ main.cpp
@@ -10,13 +10,16 @@
 #include "Jetpack.hpp"
 #include "Background.hpp"
 #include "Menu.hpp"
+#include "MenuInputHandler.hpp"
+
+#include "Modes.hpp"
 #include <ctime>
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 char *WINDOW_NAME = (char *)"Disaster at the 5C's in 2200";
 SDLHelper helper(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME);
-enum modes { MENU, CONTROLS, GAMEPLAY, ENDGAME };
+// enum modes { MENU, CONTROLS, GAMEPLAY, ENDGAME };
 
 #undef main
 int main() {
@@ -25,10 +28,10 @@ int main() {
   // Flag to close the game
   bool quit = false;
   // Initialize backgrounds
-  Background menubg(MENU, helper.renderer);
-  Background controls(CONTROLS, helper.renderer);
-  Background gameplay(GAMEPLAY, helper.renderer);
-  Background endgame(ENDGAME, helper.renderer);
+  Background menubg(game_modes::MENU, helper.renderer);
+  Background controls(game_modes::CONTROLS, helper.renderer);
+  Background gameplay(game_modes::GAMEPLAY, helper.renderer);
+  Background endgame(game_modes::ENDGAME, helper.renderer);
 
   Star star1(50, 50, helper.renderer);
   Star star2(200, 400, helper.renderer);
@@ -42,7 +45,7 @@ int main() {
   Menu menu;
   menu.render(helper.renderer);
 
-  int mode = MENU;
+  int game_mode = game_modes::MENU;
   Vec2D start_loc(10, 10);
   Vec2D vel(10, 10);
   Player p1(start_loc, vel, 100, helper.renderer, &map);
@@ -50,6 +53,7 @@ int main() {
   // Event handler
   SDL_Event e;
   InputHandler input;
+  MenuInputHandler menu_input(&game_mode, &menu);
 
   while (!quit) {
     while (SDL_PollEvent(&e) != 0) {
@@ -57,10 +61,17 @@ int main() {
       if (e.type == SDL_QUIT) {
         quit = true;
       } else {
-        Command *command = input.handle_input(e, mode == GAMEPLAY);
-        if (command != nullptr) {
-          command->execute(p1);
-        }
+          if (game_mode == game_modes::MENU) {
+              Command *command = menu_input.handle_input(e);
+              if (command != nullptr) {
+                  command->execute();
+              }
+          } else if (game_mode == game_modes::GAMEPLAY) {
+              Command *command = input.handle_input(e);
+              if (command != nullptr) {
+                command->execute(p1);
+              }
+          }
       }
     }
 
@@ -68,7 +79,7 @@ int main() {
     SDL_RenderClear(helper.renderer);
 
     // displaying backgrounds, should eventually be in menu class probably
-    if (mode == MENU) {
+    if (game_mode == game_modes::MENU) {
       menubg.render(helper.renderer);
       menu.render(helper.renderer);
       star1.update();
@@ -76,17 +87,17 @@ int main() {
       star1.render(helper.renderer);
       star2.render(helper.renderer);
     }
-    else if (mode == CONTROLS) {
+    else if (game_mode == game_modes::CONTROLS) {
       controls.render(helper.renderer);
     }
-    else if (mode == GAMEPLAY) {
+    else if (game_mode == game_modes::GAMEPLAY) {
       p1.update();
       gameplay.render(helper.renderer);
       p1.render(helper.renderer);
       map.get_obstacle_list()->render(helper.renderer);
       map.get_grappling_point_list()->render(helper.renderer);
     }
-    else if (mode == ENDGAME) {
+    else if (game_mode == game_modes::ENDGAME) {
       endgame.render(helper.renderer);
     }
 
