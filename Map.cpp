62487cc04cd6f51a37adb3c25505f58a391beccc
scsec <marmiteisgood@gmail.com>
2019-05-07 12:00:23 -0700

    Cut in quarter the amount of fuel that is spawned

--- Map.cpp
+++ Map.cpp
@@ -160,7 +160,7 @@ void Map::update_depots_and_fuel(SDL_Renderer* renderer, const Player& player) {
   if (!m_obstacle_list->m_depots.empty()) {
     // Spew fuel from all depots
     for (auto const& depot : m_obstacle_list->m_depots) {
-      if ((rand() % 100) % 2 == 0) {
+      if ((rand() % 100) % 4 == 0) {
       m_fuel_list.push_back(
           Fuel("apple.png", 1, 1, renderer,
                {depot.m_loc.x, depot.m_loc.y, Fuel::WIDTH, Fuel::HEIGHT},
